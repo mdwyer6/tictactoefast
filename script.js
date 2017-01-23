020a7b1661ee5bebc1798b0edac6b951e218c3b2
Michael Dwyer <mcdwyer1@gmail.com>
2017-01-23 15:07:58 -0800

    Improve move function logic

--- script.js
+++ script.js
@@ -21,10 +21,30 @@ Game.prototype.announceTurn() {
 Game.prototype.move(playerSign, location) {
   if (playerSign !== this.currentTurn) {
     console.log("Sorry, it isn't quite your turn yet");
+  } else if (this.board[location[0]][location[1]] !== ' . ') {
+    console.log('That space has already been taken');
   } else {
     this.board[location[0]][location[1]] = playerSign;
-
+    if (this.checkForWin()) {
+      return;
+    }
     this.draw();
     this.announceTurn();
   }
+}
+
+Game.prototype.endGame() {
+  this.move = function() {
+    console.log("The game has ended");
+  }
+}
+
+Game.prototype.checkForWin() {
+  for (var i = 0; i < this.board.length; i++) {
+    for (var j = 0; j < this.board[i].length; j++) {
+      this.board
+    }
+  }
+
+  console.log()
 }
