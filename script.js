0aa982894a02dc9002db37f0c9a07067372c50dc
Michael Dwyer <mcdwyer1@gmail.com>
2017-01-23 14:55:42 -0800

    Create move method

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ Game.prototype.announceTurn() {
   console.log(this.currentTurn);
   this.currentTurn === "X" ? this.currentTurn = "O" : this.currentTurn = "X";
 }
+
+
+Game.prototype.move(playerSign, location) {
+  if (playerSign !== this.currentTurn) {
+    console.log("Sorry, it isn't quite your turn yet");
+  } else {
+    this.board[location[0]][location[1]] = playerSign;
+
+    this.draw();
+    this.announceTurn();
+  }
+}
