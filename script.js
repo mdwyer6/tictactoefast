2b85c48fd61c79240136f8fdcdbfb75b39fd6436
Michael Dwyer <mcdwyer1@gmail.com>
2017-01-23 14:54:52 -0800

    Create draw method

--- script.js
+++ script.js
@@ -11,19 +11,3 @@ var Game = function() {
 Game.prototype.draw() {
   console.log(this.board);
 }
-
-Game.prototype.announceTurn() {
-  console.log(this.currentTurn);
-  this.currentTurn === "X" ? this.currentTurn = "O" : this.currentTurn = "X";
-}
-
-Game.prototype.move(playerSign, location) {
-  if (playerSign !== this.currentTurn) {
-    console.log("Sorry, it isn't quite your turn yet");
-  } else {
-    this.board[location[0]][location[1]] = playerSign;
-    this.draw();
-    this.announceTurn();
-  }
-}
-
